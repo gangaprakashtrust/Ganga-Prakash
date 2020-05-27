01bf17a78775cb486f7e2bef3931562199f7a7e9
abhishekd9519 <abhishekd9519@gmail.com>
2020-05-27 10:44:02 +0530

    Create GangaPrakashAPI.Model solution and added Base Class in system folder.

---
+++ Model/GangaPrakashAPI.Model/GangaPrakashAPI.Model/System/Business/Basic/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangaPrakashAPI.Model
+{
+    public class BaseModel
+    {
+        public Guid Id { get; set; }
+
+        public Boolean IsError { get; set; }
+
+        public String ErrorMessageFor { get; set; }
+
+        public String ErrorMessage { get; set; }
+
+        public Guid TransactedBy { get; set; }
+    }
+}
